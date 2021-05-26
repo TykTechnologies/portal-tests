875dbdf2aeab5fb395517ff8002c3c8cd9bebd8a
Konrad Soltys <konrad@macbook-pro-konrad.home>
2021-05-26 15:11:35 +0200

    add test for initial service

---
+++ test/specs/services/initial_services_test.js
@@ -0,0 +1,17 @@
+import { login_page } from '../../../lib/pom/Login_page';
+import { admin_page } from '../../../lib/pom/Admin_page';
+
+describe('Service was loaded on the bootstraping', () => {
+  const serviceName = 'raava service 1';
+
+  before(() => {
+    login_page.open();
+    login_page.login();
+  });
+
+  it('Admin should be able to see service that was loaded on bootstraping', () => {
+    admin_page.SERVICES_BUTTON.click();
+    wdioExpect($(`div=${serviceName}`)).toBeDisplayed();
+  });
+  
+});
