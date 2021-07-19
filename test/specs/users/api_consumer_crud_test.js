6adc7af83b1f686f28aea390d7ead56a46f9cbc9
Konrad Soltys <konrad@macbook-pro-konrad.home>
2021-07-19 14:51:32 +0200

    update tests for admin users and API consumers

---
+++ test/specs/users/api_consumer_crud_test.js
@@ -0,0 +1,47 @@
+import { login_page } from '../../../lib/pom/Login_page';
+import { admin_page } from '../../../lib/pom/Admin_page';
+import { api_consumers_page } from '../../../lib/pom/Api_consumers_page';
+
+describe('Creating new users', () => {
+  const userDetails = {
+    first: "test_first_name",
+    update_first: "update_first",
+    last: "test_second_name",
+    email: "api_consumer_email@tyk.io",
+    password: "test123",
+    update_password: "test321",
+    isActive: true,
+    org: "Default Organisation"
+  };
+
+  before(() => {
+    login_page.open();
+    login_page.login();
+  });
+
+  it('Admin should be able to create a developer', () => {
+    admin_page.API_CONSUMERS_BUTTON.click();
+    api_consumers_page.ADD_BUTTON.click();
+    api_consumers_page.fillNewUserForm(userDetails);
+    api_consumers_page.SAVE_BUTTON.click();
+  });
+
+  it('New user is visible in the user table', () => {
+    wdioExpect($(`div=${userDetails.email}`)).toBeDisplayed();
+  });
+
+  it('Admin should be able to update a developer', () => {
+    api_consumers_page.TABLE.clickCellWithText(userDetails.email);
+    api_consumers_page.FIRST_NAME_INPUT.setValue(userDetails.update_first);
+    api_consumers_page.PASSWORD_INPUT.setValue(userDetails.update_password);
+    api_consumers_page.SAVE_CHANGES_BUTTON.click();
+    wdioExpect($(`div=${userDetails.update_first}`)).toBeDisplayed();
+  });
+
+  it('Admin is able to delete developer', () => {
+    api_consumers_page.TABLE.deleteRow(1);
+    const wasRowDeleted = api_consumers_page.TABLE.isCellWithTextNotDisplayed(userDetails.name_update);
+    expect(wasRowDeleted).to.be.true;
+  });
+  
+});
