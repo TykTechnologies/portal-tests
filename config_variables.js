1d6f46e542904afa5eb4895f836baec90d3c1cb0
Konrad Soltys <konrad@macbook-pro-konrad.home>
2021-07-28 13:20:58 +0200

    CI changes and provider test

--- config_variables.js
+++ config_variables.js
@@ -4,10 +4,15 @@ const URL = process.env.URL || "http://localhost:3001/";
 
 module.exports = {
     URL: URL,
+    GW_URL: process.env.GW_URL || "http://localhost:8085/",
     LOGIN_PATH: "auth/login",
     DASHBOARD_API: URL + 'api/',
     DASHBOARD_ADMIN_API: URL + 'admin/',
 
     USER_EMAIL: process.env.USER_EMAIL || "auto_test@tyk.io",
     USER_PASSWORD: process.env.USER_PASSWORD || "test123",
+
+    TYK_SECRET: "2bc29a1f2bed4a5f5554f0b457789ed0",
+    TYK_ORG_ID: "61010bc82ad0a20001fc35b5",
+    TYK_GW_SECRET: "352d20ee67be67f6340b4c0605b044b7"
 };
---
+++ test/specs/providers/provider_synchronize_test.js
@@ -0,0 +1,46 @@
+import { login_page } from '../../../lib/pom/Login_page';
+import { admin_page } from '../../../lib/pom/Admin_page';
+import { Gateway_connection } from '../../../lib/gateway_connection';
+import { providers_page } from '../../../lib/pom/Providers_page';
+import { TYK_ORG_ID, TYK_SECRET } from '../../../config_variables';
+
+describe('Synchronize with Tyk Pro', () => {
+  const expectedCountOfServices = "1";
+  const expectedCountOfTiers = "1";
+  const providerDetails = {
+    name: "tyk provider",
+    type: "tyk-pro",
+    metaData: `{"URL": "http://localhost:3005", "Secret": "${TYK_SECRET}", "OrgID": "${TYK_ORG_ID}"}`,
+  };
+
+  before(() => {
+    const gw_connection = new Gateway_connection();
+    gw_connection.waitUntilGWisUp();
+    login_page.open();
+    login_page.login();
+  });
+
+  it('Admin should be able to update default provider with proper Tyk details', () => {
+    admin_page.PROVIDERS_BUTTON.click();
+    providers_page.TABLE.clickCellWithText("tyk-pro");
+    providers_page.EDIT_BUTTON.click();
+    providers_page.NAME_INPUT.waitForClickable();
+    providers_page.NAME_INPUT.setValue(providerDetails.name);
+    providers_page.METADATA_INPUT.setValue(providerDetails.metaData);
+    providers_page.SAVE_CHANGES_BUTTON.click();
+  });
+
+  it('New connection to Tyk is visible in the org table', () => {
+    wdioExpect($(`div=${providerDetails.name}`)).toBeDisplayed();
+  });
+
+  it('Admin is able synchronize the services', () => {
+    providers_page.SYNCHRONIZE_BUTTON.click();
+    providers_page.DIALOG_OK_BUTTON.click();
+    const servicesCell = providers_page.getTabelServiceCellFromRow(1);
+    const tiersCell = providers_page.getTabelServiceCellFromRow(1);
+    wdioExpect(servicesCell).toHaveText(expectedCountOfServices);
+    wdioExpect(tiersCell).toHaveText(expectedCountOfTiers);
+  });
+  
+});
