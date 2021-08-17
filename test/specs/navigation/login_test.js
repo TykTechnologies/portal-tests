e07727d2e224fe184a335cd954aaf1e136686b30
Patricio Diaz <padiazg@gmail.com>
2021-08-17 17:36:30 -0400

    Updated test to reflect actual changes to the UI
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

--- test/specs/navigation/login_test.js
+++ test/specs/navigation/login_test.js
@@ -3,35 +3,36 @@ import { admin_page } from '../../../lib/pom/Admin_page';
 import { main_page } from '../../../lib/pom/Main_page';
 
 describe('Login/logout check', () => {
- 
-  before(() => {
-    login_page.open();
-  });
 
-  it('User should be able to login using admin credentials', () => {
-    login_page.login();
-    admin_page.checkIfPageIsOpened();
-  });
+    before(() => {
+        login_page.open();
+    });
 
-  it('Main Admin page should be displayed after login', () => {
-      wdioExpect(admin_page.SECTION_TITLE_TEXT).toHaveText("Admin");
-  });
+    it('User should be able to login using admin credentials', () => {
+        login_page.login();
+        admin_page.checkIfPageIsOpened();
+    });
 
-  it('Admin is able to open dashboard', () => {
-    admin_page.openDashboard();
-    wdioExpect(main_page.APIS_BUTTON).toBeDisplayed();
-  });
+    it('Main Admin page should be displayed after login', () => {
+        wdioExpect(admin_page.SECTION_TITLE_TEXT).toHaveText("Admin");
+    });
 
-  it('Admin is able to logout', () => {
-      browser.pause(1000);
-      main_page.LOG_OUT_BUTTON.click();
-      wdioExpect(main_page.LOG_IN_BUTTON).toBeDisplayed();
-      wdioExpect(main_page.REGISTER_BUTTON).toBeDisplayed();
-  });
+    it('Admin is able to open dashboard', () => {
+        admin_page.openDashboard();
+        wdioExpect(main_page.APIS_BUTTON).toBeDisplayed();
+    });
 
-  it('Users stays logout after refresh', () => {
-    browser.refresh();
-    wdioExpect(main_page.REGISTER_BUTTON).toBeDisplayed();
-    wdioExpect(main_page.LOG_IN_BUTTON).toBeDisplayed();
-  });
+    it('Admin is able to logout', () => {
+        browser.pause(1000);
+        main_page.USER_PROFILE_DROPDOWN.click()
+        main_page.LOG_OUT_BUTTON.click();
+        wdioExpect(main_page.LOG_IN_BUTTON).toBeDisplayed();
+        wdioExpect(main_page.REGISTER_BUTTON).toBeDisplayed();
+    });
+
+    it('Users stays logout after refresh', () => {
+        browser.refresh();
+        wdioExpect(main_page.REGISTER_BUTTON).toBeDisplayed();
+        wdioExpect(main_page.LOG_IN_BUTTON).toBeDisplayed();
+    });
 });
--- test/specs/users/admin_user_crud_test.js
+++ test/specs/users/admin_user_crud_test.js
@@ -3,44 +3,44 @@ import { admin_page } from '../../../lib/pom/Admin_page';
 import { admin_users_page } from '../../../lib/pom/Admin_users_page';
 
 describe('Creating new users', () => {
-  const userDetails = {
-    first: "test_first_name",
-    update_first: "update_first_name",
-    last: "test_second_name",
-    email: "test_email@tyk.io",
-    password: "test123",
-    update_password: "test321",
-    isActive: true
-  };
+    const userDetails = {
+        first: "test_first_name",
+        update_first: "update_first_name",
+        last: "test_second_name",
+        email: "test_email@tyk.io",
+        // password: "test123",
+        // update_password: "test321",
+        isActive: true
+    };
 
-  before(() => {
-    login_page.open();
-    login_page.login();
-  });
+    before(() => {
+        login_page.open();
+        login_page.login();
+    });
 
-  it('Admin should be able to create a provider-admin', () => {
-    admin_page.ADMIN_USERS_BUTTON.click();
-    admin_users_page.ADD_BUTTON.click();
-    admin_users_page.fillNewUserForm(userDetails);
-    admin_users_page.SAVE_BUTTON.click();
-  });
+    it('Admin should be able to create a provider-admin', () => {
+        admin_page.ADMIN_USERS_BUTTON.click();
+        admin_users_page.ADD_BUTTON.click();
+        admin_users_page.fillNewUserForm(userDetails);
+        admin_users_page.SAVE_BUTTON.click();
+    });
 
-  it('New user is visible in the user table', () => {
-    wdioExpect($(`div=${userDetails.email}`)).toBeDisplayed();
-  });
+    it('New user is visible in the user table', () => {
+        wdioExpect($(`div=${userDetails.email}`)).toBeDisplayed();
+    });
 
-  it('Admin should be able to update a provider-admin account', () => {
-    admin_users_page.TABLE.clickCellWithText(userDetails.email);
-    admin_users_page.FIRST_NAME_INPUT.setValue(userDetails.update_first);
-    admin_users_page.PASSWORD_INPUT.setValue(userDetails.update_password);
-    admin_users_page.SAVE_CHANGES_BUTTON.click();
-    wdioExpect($(`div=${userDetails.update_first}`)).toBeDisplayed();
-  });
+    it('Admin should be able to update a provider-admin account', () => {
+        admin_users_page.TABLE.clickCellWithText(userDetails.email);
+        admin_users_page.FIRST_NAME_INPUT.setValue(userDetails.update_first);
+        // admin_users_page.PASSWORD_INPUT.setValue(userDetails.update_password);
+        admin_users_page.SAVE_CHANGES_BUTTON.click();
+        wdioExpect($(`div=${userDetails.update_first}`)).toBeDisplayed();
+    });
 
-  it('Admin is able to delete developer', () => {
-    admin_users_page.TABLE.deleteRow(1);
-    const wasRowDeleted = admin_users_page.TABLE.isCellWithTextNotDisplayed(userDetails.name_update);
-    expect(wasRowDeleted).to.be.true;
-  });
-  
+    it('Admin is able to delete developer', () => {
+        admin_users_page.TABLE.deleteRow(1);
+        const wasRowDeleted = admin_users_page.TABLE.isCellWithTextNotDisplayed(userDetails.name_update);
+        expect(wasRowDeleted).to.be.true;
+    });
+
 });
