add68d7cfcfb3bc5868d5457a4d845f22d5ea691
konrad <konrad@tyk.io>
2021-12-16 17:22:18 +0100

    rename portal to dev

--- test/flowSteps/using_token.js
+++ test/flowSteps/using_token.js
@@ -1,7 +1,6 @@
 import { login_page } from '../../lib/pom/Login_page';
 import { portal_page } from '../../lib/pom/Portal_page';
 import { GW_connection } from '../../lib/GW_connection';
-import { portal_apps_page } from '../../lib/pom/Portal_apps_page';
 import { PUBLIC_API_NAME } from '../../config_variables';
 import { dev_apps_page } from '../../lib/pom/Dev_apps_page';
 
--- test/specs/catalogues/private_catalogue_test.js
+++ test/specs/catalogues/private_catalogue_test.js
@@ -1,5 +1,5 @@
 import { login_page } from '../../../lib/pom/Login_page';
-import { portal_catalogues_page } from '../../../lib/pom/Portal_catalogues_page';
+import { dev_catalogues_page } from '../../../lib/pom/Dev_catalogues_page';
 import { main_page } from '../../../lib/pom/Main_page';
 import { PRODUCT_PUBLIC_NAME, PRODUCT_PRIVATE_NAME } from '../../../config_variables';
 
@@ -13,9 +13,9 @@ describe('Private catalogue', () => {
 
     it('Logged user can see Public and Private Catalogue products', () => {
         main_page.CATALOGUES_BUTTON.click();
-        const cataloguesCardsArray = portal_catalogues_page.getAllDisplayedProducts();
+        const cataloguesCardsArray = dev_catalogues_page.getAllDisplayedProducts();
         expect(cataloguesCardsArray).to.have.lengthOf(expectedCountOfVisibleProducts, "Incorrect number of visible catalogues!");
-        const cataloguesNames = portal_catalogues_page.getNamesOfDisplayedProducts();
+        const cataloguesNames = dev_catalogues_page.getNamesOfDisplayedProducts();
         expect(cataloguesNames).to.have.members([PRODUCT_PUBLIC_NAME, PRODUCT_PRIVATE_NAME]);
     });
 });
