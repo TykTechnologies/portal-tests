737423320d63b348aab30f2aee87b9a3534859f3
konrad <konrad@tyk.io>
2022-01-12 15:02:11 +0100

    fix in limit test

--- test/specs/provisioning_request/token_limits_flow.js
+++ test/specs/provisioning_request/token_limits_flow.js
@@ -1,6 +1,5 @@
 import { DEV_A1_EMAIL, DEV_PASS} from '../../../config_variables';
-browser.pause(60000)
-const appNameLimits = require('../../flowSteps/request_access')(DEV_A1_EMAIL, DEV_PASS);
+const appNameLimits = require('../../flowSteps/request_access_limits')(DEV_A1_EMAIL, DEV_PASS);
 console.log(">>> Provisioning request created in: " + appNameLimits);
 require('../../flowSteps/approve_requets')(appNameLimits);
 require('../../flowSteps/token_limits')(appNameLimits);
