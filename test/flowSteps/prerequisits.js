3ad0e39f6885bfd9d5fc0ab5d0c9b96d54ef3222
konrad <konrad@tyk.io>
2022-01-20 09:39:53 +0100

    update portal.db with new column name

--- test/flowSteps/prerequisits.js
+++ test/flowSteps/prerequisits.js
@@ -16,7 +16,7 @@ describe('Prerequisits', () => {
     [d.ORG_A_NAME, d.TEAM_A1_NAME], [d.ORG_B_NAME, d.TEAM_B_NAME]];
   let devsList = {
     email: [d.DEV_EMAIL, d.DEV_A_EMAIL, d.DEV_A1_EMAIL, d.DEV_B_EMAIL],
-    first: ["default", "teamDefaultA", "teamA", "teamB"],
+    first: ["default", "teamA", "teamA1", "teamB"],
     last:["default", "orgsA", "orgsA", "orgB"],
     password: [d.DEV_PASS, d.DEV_PASS, d.DEV_PASS, d.DEV_PASS],
     inviteCode: ["", "", "", ""]
@@ -62,109 +62,109 @@ describe('Prerequisits', () => {
     login_page.login();
   });
 
-  orgsList.forEach( orgName => {
-    it('Creating org ' + orgName, () => {
-      browser.refresh();
-      admin_page.ORGANIATIONS_BUTTON.click();
-      org_page.ADD_BUTTON.click();
-      org_page.NAME_INPUT.setValue(orgName);
-      org_page.SUBMIT_NEW_BUTTON.click();
-    });
-  });
+//   orgsList.forEach( orgName => {
+//     it('Creating org ' + orgName, () => {
+//       browser.refresh();
+//       admin_page.ORGANIATIONS_BUTTON.click();
+//       org_page.ADD_BUTTON.click();
+//       org_page.NAME_INPUT.setValue(orgName);
+//       org_page.SUBMIT_NEW_BUTTON.click();
+//     });
+//   });
 
-  teamsList.forEach( team => {
-    it('Creating a Team ' + team[1], () => {
-      browser.refresh();
-      admin_page.TEAMS_BUTTON.click();
-      teams_page.ADD_BUTTON.click();
-      teams_page.NAME_INPUT.setValue(team[1]);
-      teams_page.ORG_DROPDOWN.selectOption(team[0]);
-      teams_page.SUBMIT_NEW_BUTTON.click();
-    });
-  });
+//   teamsList.forEach( team => {
+//     it('Creating a Team ' + team[1], () => {
+//       browser.refresh();
+//       admin_page.TEAMS_BUTTON.click();
+//       teams_page.ADD_BUTTON.click();
+//       teams_page.NAME_INPUT.setValue(team[1]);
+//       teams_page.ORG_DROPDOWN.selectOption(team[0]);
+//       teams_page.SUBMIT_NEW_BUTTON.click();
+//     });
+//   });
   
-  it('Creating invite code inviteCodeA', () => {
-    admin_page.INVITE_CODES_BUTTON.click();
-    invite_codes_page.ADD_BUTTON.click();
-    invite_codes_page.fillNewInviteCodeForm(inviteCodeA);
-    invite_codes_page.SAVE_BUTTON.click();
-    browser.pause(2000);
-    devsList.inviteCode[1] = invite_codes_page.getCodeFromRow(0);
-    console.log(`Invite code created: ${devsList.inviteCode[1]}`);
-   });
+//   it('Creating invite code inviteCodeA', () => {
+//     admin_page.INVITE_CODES_BUTTON.click();
+//     invite_codes_page.ADD_BUTTON.click();
+//     invite_codes_page.fillNewInviteCodeForm(inviteCodeA);
+//     invite_codes_page.SAVE_BUTTON.click();
+//     browser.pause(2000);
+//     devsList.inviteCode[1] = invite_codes_page.getCodeFromRow(0);
+//     console.log(`Invite code created: ${devsList.inviteCode[1]}`);
+//    });
 
-   it('Creating invite code inviteCodeA1', () => {
-    admin_page.INVITE_CODES_BUTTON.click();
-    invite_codes_page.ADD_BUTTON.click();
-    invite_codes_page.fillNewInviteCodeForm(inviteCodeA1);
-    invite_codes_page.SAVE_BUTTON.click();
-    browser.pause(2000);
-    devsList.inviteCode[2] = invite_codes_page.getCodeFromRow(0);
-    console.log(`Invite code created: ${devsList.inviteCode[2]}`);
-   });
+//    it('Creating invite code inviteCodeA1', () => {
+//     admin_page.INVITE_CODES_BUTTON.click();
+//     invite_codes_page.ADD_BUTTON.click();
+//     invite_codes_page.fillNewInviteCodeForm(inviteCodeA1);
+//     invite_codes_page.SAVE_BUTTON.click();
+//     browser.pause(2000);
+//     devsList.inviteCode[2] = invite_codes_page.getCodeFromRow(0);
+//     console.log(`Invite code created: ${devsList.inviteCode[2]}`);
+//    });
 
-   it('Creating invite code inviteCodeB', () => {
-    admin_page.INVITE_CODES_BUTTON.click();
-    invite_codes_page.ADD_BUTTON.click();
-    invite_codes_page.fillNewInviteCodeForm(inviteCodeB1);
-    invite_codes_page.SAVE_BUTTON.click();
-    browser.pause(2000);
-    devsList.inviteCode[3] = invite_codes_page.getCodeFromRow(0);
-    console.log(`Invite code created: ${devsList.inviteCode[3]}`);
-   });
+//    it('Creating invite code inviteCodeB', () => {
+//     admin_page.INVITE_CODES_BUTTON.click();
+//     invite_codes_page.ADD_BUTTON.click();
+//     invite_codes_page.fillNewInviteCodeForm(inviteCodeB1);
+//     invite_codes_page.SAVE_BUTTON.click();
+//     browser.pause(2000);
+//     devsList.inviteCode[3] = invite_codes_page.getCodeFromRow(0);
+//     console.log(`Invite code created: ${devsList.inviteCode[3]}`);
+//    });
 
-   it('creating users', () => {
-    admin_page.logOut();
-    login_page.open();
-    for (let i =0; i <4; i++) {
-      let registrationDetailsWithCode = {
-        email: devsList.email[i],
-        first: devsList.first[i],
-        last: devsList.last[i],
-        password: devsList.password[i],
-        inviteCode: devsList.inviteCode[i]
-      };
-      main_page.REGISTER_BUTTON.click();
-      registration_page.fillForm(registrationDetailsWithCode);
-      registration_page.REGISTER_BUTTON.click();
-      wdioExpect(main_page.REGISTRATION_SUCCESS_LABEL).toBeDisplayed();
-     }
-   });
+//    it('creating users', () => {
+//     admin_page.logOut();
+//     login_page.open();
+//     for (let i =0; i <4; i++) {
+//       let registrationDetailsWithCode = {
+//         email: devsList.email[i],
+//         first: devsList.first[i],
+//         last: devsList.last[i],
+//         password: devsList.password[i],
+//         inviteCode: devsList.inviteCode[i]
+//       };
+//       main_page.REGISTER_BUTTON.click();
+//       registration_page.fillForm(registrationDetailsWithCode);
+//       registration_page.REGISTER_BUTTON.click();
+//       wdioExpect(main_page.REGISTRATION_SUCCESS_LABEL).toBeDisplayed();
+//      }
+//    });
 
-it('Admin should be able to ADD provider with proper Tyk details', () => {
-  login_page.open();
-  login_page.login();
-  admin_page.PROVIDERS_BUTTON.click();
-  if ($('div*=tyk-pro').isDisplayed()) {
-    providers_page.TABLE.deleteRow(0);
-  }
-  providers_page.ADD_BUTTON.click();
-  providers_page.NAME_INPUT.waitForClickable();
-  providers_page.NAME_INPUT.setValue(providerDetails.name);
-  providers_page.URL_INPUT.setValue(providerDetails.url);
-  providers_page.SECRET_INPUT.setValue(providerDetails.secret);
-  providers_page.ORG_ID_INPUT.setValue(providerDetails.org_id);
-  providers_page.SAVE_BUTTON.click();
-  wdioExpect(providers_page.TABLE.$(`div*=${providerDetails.name}`)).toBeDisplayed();    
-  providers_page.SYNCHRONIZE_BUTTON.click();
-  providers_page.DIALOG_OK_BUTTON.click();
-});
+// it('Admin should be able to ADD provider with proper Tyk details', () => {
+//   login_page.open();
+//   login_page.login();
+//   admin_page.PROVIDERS_BUTTON.click();
+//   if ($('div*=tyk-pro').isDisplayed()) {
+//     providers_page.TABLE.deleteRow(0);
+//   }
+//   providers_page.ADD_BUTTON.click();
+//   providers_page.NAME_INPUT.waitForClickable();
+//   providers_page.NAME_INPUT.setValue(providerDetails.name);
+//   providers_page.URL_INPUT.setValue(providerDetails.url);
+//   providers_page.SECRET_INPUT.setValue(providerDetails.secret);
+//   providers_page.ORG_ID_INPUT.setValue(providerDetails.org_id);
+//   providers_page.SAVE_BUTTON.click();
+//   wdioExpect(providers_page.TABLE.$(`div*=${providerDetails.name}`)).toBeDisplayed();    
+//   providers_page.SYNCHRONIZE_BUTTON.click();
+//   providers_page.DIALOG_OK_BUTTON.click();
+// });
 
-  it('edit public catalogue', () => {
-    admin_page.CATALOGUES_BUTTON.click();
-    catalogues_page.TABLE.clickCellWithText("Public Catalogue");
-    catalogues_page.PRODUCTS_DROPDOWN.selectOption(d.PRODUCT_PUBLIC_NAME);
-    catalogues_page.PLANS_DROPDOWN.selectOption(d.FREE_PLAN_NAME);
-    catalogues_page.SAVE_CHANGES_BUTTON.click();
-  });
+//   it('edit public catalogue', () => {
+//     admin_page.CATALOGUES_BUTTON.click();
+//     catalogues_page.TABLE.clickCellWithText("Public Catalogue");
+//     catalogues_page.PRODUCTS_DROPDOWN.selectOption(d.PRODUCT_PUBLIC_NAME);
+//     catalogues_page.PLANS_DROPDOWN.selectOption(d.FREE_PLAN_NAME);
+//     catalogues_page.SAVE_CHANGES_BUTTON.click();
+//   });
 
-  it('edit private catalogue', () => {
-    admin_page.CATALOGUES_BUTTON.click();
-    catalogues_page.TABLE.clickCellWithText("Private Catalogue");
-    catalogues_page.PRODUCTS_DROPDOWN.selectOption(d.PRODUCT_PRIVATE_NAME);
-    catalogues_page.PLANS_DROPDOWN.selectOption(d.BRONZE_PLAN_NAME);
-    catalogues_page.SAVE_CHANGES_BUTTON.click();
-  });
+//   it('edit private catalogue', () => {
+//     admin_page.CATALOGUES_BUTTON.click();
+//     catalogues_page.TABLE.clickCellWithText("Private Catalogue");
+//     catalogues_page.PRODUCTS_DROPDOWN.selectOption(d.PRODUCT_PRIVATE_NAME);
+//     catalogues_page.PLANS_DROPDOWN.selectOption(d.BRONZE_PLAN_NAME);
+//     catalogues_page.SAVE_CHANGES_BUTTON.click();
+//   });
 
   it('add catalogue org A', () => {
     admin_page.CATALOGUES_BUTTON.click();
