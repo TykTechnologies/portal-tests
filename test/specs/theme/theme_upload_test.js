badcf81e3ce3ea4803402b54257d5c896de4c097
konrad <konrad@tyk.io>
2022-04-06 14:56:47 +0200

    add test for making theme current

--- test/specs/theme/theme_upload_test.js
+++ test/specs/theme/theme_upload_test.js
@@ -1,18 +1,22 @@
 import { login_page } from '../../../lib/pom/Login_page';
 import { admin_page } from '../../../lib/pom/Admin_page';
 import { themes_page } from '../../../lib/pom/Themes_page';
+import { dev_catalogues_page } from '../../../lib/pom/Dev_catalogues_page';
+import { URL } from '../../../config_variables';
 const path = require('path');
 
-describe('Uploading theme and make it active', () => {
+describe('Uploading theme and making it active', () => {
   const zipFileRelativePath = "./test_theme.zip";
   const themeName = 'test theme';
+  const defaultThemeName = 'default';
+  const cataloguesURLPath = URL + "portal/catalogue-products";
   
   before(() => {
     login_page.open();
     login_page.login();
   });
 
-  it('Admin should be able upload new Theme', () => {
+  it('Admin should be able to upload new Theme', () => {
     admin_page.THEMES_BUTTON.click();
     //make sure that theme is not already there
     themes_page.TABLE.expectCellWithTextNotToBeDisplayed(themeName);
@@ -34,6 +38,21 @@ describe('Uploading theme and make it active', () => {
     themes_page.activateTheme(themeName);
     expect(themes_page.isThemeActive(themeName)).to.be.true;
   });
+
+  it('Changes in current theme are visible in UI', () => {
+    browser.url(cataloguesURLPath);
+    wdioExpect($('h1*=Product Catalogues Test Theme')).toBeDisplayed(); //"Test Theme" was added in theme
+  });
+
+  it('Admin is able to change change theme back to default', () => {
+    browser.back();
+    themes_page.activateTheme(defaultThemeName);
+    browser.url(cataloguesURLPath);
+    wdioExpect($('h1*=Product Catalogues Test Theme')).not.toBeDisplayed();
+    wdioExpect($('h1*=Product Catalogues')).toBeDisplayed();
+  });
+
+
 
   const uploadZipFile = () => {
     const filePath = path.join(__dirname, zipFileRelativePath);
