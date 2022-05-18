6a3e3e6bb36f164b24776b6be3c60b0efbcd8a52
konrad <konrad@tyk.io>
2022-05-18 11:52:20 +0200

    update xray

--- update_xray.sh
+++ update_xray.sh
@@ -1,23 +1,22 @@
 echo "Sending update of test execution to Xray"
-EXECUTION=QA-895
 if [ "$STATUS" == "success" ]; then
     RESULT="PASSED"
 else
     RESULT="FAILED"
 fi
-if [ "$BRANCH" == "success" ]; then
-    STATUS="PASSED"
-else
-    STATUS="FAILED"
+echo "Checking branch: $BRANCH"
+if [ "$BRANCH" == "xray" ]; then
+    EXECUTION=QA-895
 fi
 
-echo "Branch variable:"
-echo "$BRANCH"
-echo "CLI:"
-echo "$5"
-echo "Requesting Token"
-TOKEN=$(curl -H "Content-Type: application/json" -X POST --data "{ \"client_id\": \"$CLIENT_ID\",\"client_secret\": \"$CLIENT_SECRET\" }" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
-echo "Sending update $RESULT on $TEST and execution $EXECUTION"
-curl -H "Authorization: Bearer $TOKEN" -X POST https://xray.cloud.getxray.app/api/v2/import/execution \
-    -d '{"testExecutionKey":"'$EXECUTION'","tests":[{"testKey":"'$TEST'","status":"'$RESULT'"}]}' \
-    -H 'Content-Type: application/json'
+if [[ ! -z "$EXECUTION" ]]; then
+    echo "Requesting Token"
+    TOKEN=$(curl -H "Content-Type: application/json" -X POST --data "{ \"client_id\": \"$CLIENT_ID\",\"client_secret\": \"$CLIENT_SECRET\" }" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
+    echo "Sending update $RESULT on $TEST and execution $EXECUTION"
+    curl -H "Authorization: Bearer $TOKEN" -X POST https://xray.cloud.getxray.app/api/v2/import/execution \
+        -d '{"testExecutionKey":"'$EXECUTION'","tests":[{"testKey":"'$TEST'","status":"'$RESULT'"}]}' \
+        -H 'Content-Type: application/json'
+else
+    echo "Branch $BRANCH not specified for updated"
+fi
+
